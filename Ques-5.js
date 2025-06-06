1c5b48a2552afa44406e1e2eb7ce6721457f33fa
Preeti <preeti.garg@successive.tech>
2025-06-06 15:10:10 +0530

    assignment completed

---
+++ Ques-5.js
@@ -0,0 +1,18 @@
+// 5. Write a program to print all even number first and then all odd numbers using only one iteration
+
+let arr = [2, 3, 4, 5, 7, 45, 67, 12, 23, 42, 51, 22];
+function oddEvenNumber(arr) {
+  let arrEven = [];
+  let arrOdd = [];
+
+  for (let i of arr) {
+    if (i % 2 == 0) {
+      arrEven += i + " ";
+    } else {
+      arrOdd += i + " ";
+    }
+  }
+  let arrEvenOdd = arrEven + arrOdd;
+  console.log(arrEvenOdd);
+}
+oddEvenNumber(arr);
