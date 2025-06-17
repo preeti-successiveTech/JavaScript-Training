9148ad9e07e29f62b56b1ca4f6a7bcd80dd33cd2
Preeti <preeti.garg@successive.tech>
2025-06-17 10:23:00 +0530

    assignmnet-9 complete

---
+++ assignment-9/Math/math.js
@@ -0,0 +1,15 @@
+// 3.create a new folder named 'math' and create a file math.js. In math.js, define two functions:
+// a. add(a, b): takes two numbers as arguments and returns their sum.
+// b. subtract(a, b): takes two numbers as arguments and returns their difference.
+// create a new file index.js and just export those functions and then use those function in any of the file
+
+function add(a,b){
+    return a+b;
+}
+function sub(a,b)
+{
+    return (a>b)?a-b : b-a;
+}
+console.log(sub(10,25));
+module.exports = { add, sub};
+
---
+++ assignment-9/index.js
@@ -0,0 +1,5 @@
+const lodash = require("lodash");
+const { add, sub} = require('./Math/math');
+
+console.log(add(4, 4)); 
+console.log(sub(8, 4)); 
